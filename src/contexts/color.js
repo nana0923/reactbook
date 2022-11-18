f47bad58eb4526bab7727196bc87d42c77ef3a60
NaNa923 <k5279kr@gmail.com>
2022-11-18 11:04:17 +0900

    colorBox

---
+++ src/contexts/color.js
@@ -0,0 +1,31 @@
+const { createContext, useState } = require("react");
+
+const ColorContext = createContext({ 
+    state : { color: 'black', subcolor: 'red' },
+    actions: {
+        setColor : () => {},
+        setSubcolor : () => {}
+    }
+});
+
+const ColorProvider = ({ children }) => {
+    const [color, setColor] = useState('black');
+    const [subcolor, setSubcolor] = useState('red');
+
+    const value = {
+        state: { color, subcolor },
+        actions: {setColor, setSubcolor }
+    };
+    return (
+        <ColorContext.Provider value={value}>{children}</ColorContext.Provider>
+    );
+}; 
+
+// const ColorConsumer = ColorContext.Consumer와 같은 의미
+const { Consumer:ColorConsumer } = ColorContext;
+
+// ColorProvider와 ColorConsumer내보내기
+export { ColorProvider, ColorConsumer };
+
+export default ColorContext;
+
